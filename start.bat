e1c6dffcb51d375d309685ee8a191cb057c6dadf
蒋士琼 <781456366@QQ.COM>
2026-07-17 20:38:23 +0800

    Add startup batch file for Windows

---
+++ start.bat
@@ -0,0 +1,9 @@
+@echo off
+cd /d %~dp0
+echo ========================================
+echo   报关辅助系统 Customs App v2.0
+echo   启动中...
+echo ========================================
+echo.
+node server.js
+pause
